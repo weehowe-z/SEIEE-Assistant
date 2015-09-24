cfb1a49c951dee8629cd4f9c7eeb7890a132153b
weehowe-z <weehowe.z@gmail.com>
2015-09-24 23:33:10 +0800

    add new feature: pair content with name

--- Z.Seiee Crawler/zseiee.py
+++ Z.Seiee Crawler/zseiee.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*- 
 import xlwt #deal with excel
+import xlrd
 import urllib2
 
 def getInfo(student_id):
 	url_base = 'http://z.seiee.com/index.php/Score/search?sid='
-	student_id_str = str(student_id) 
-	if len(student_id_str) == 1:
-		student_id_str = '00'+student_id_str
-	elif len(student_id_str) == 2:
-		student_id_str = '0' + student_id_str
-	url = url_base + '5130309'+ student_id_str
-	page = urllib2.urlopen(url).read()
+	student_id_str = str(student_id)
+	url = url_base + student_id_str
+	try:
+		page = urllib2.urlopen(url,timeout=1).read()
+	except:
+		return None
+
 
 	name_start = page.find('<td width="70%">',0)
 	if name_start == -1:
@@ -31,8 +32,37 @@ def getInfo(student_id):
 
 	return [name,id,mark,rank]
 
-def createXls():
-	book = xlwt.Workbook(encoding='utf-8',style_compression=0)
+def dealXls():
+	book = xlrd.open_workbook('content.xls')
+	savebook = xlwt.Workbook(encoding = 'utf-8',style_compression=0)
+	for i in range (0,10):
+		sheet = book.sheet_by_index(i)
+		print "now deal with major " + sheet.name
+		savesheet =savebook.add_sheet(sheet.name,cell_overwrite_ok=True)
+		savesheet.write(0,0,'序号'.decode('utf-8'))
+		savesheet.write(0,1,'学号'.decode('utf-8'))
+		savesheet.write(0,2,'专业'.decode('utf-8'))
+		savesheet.write(0,3,'第一学期综合测评成绩'.decode('utf-8'))
+		savesheet.write(0,4,'第二学期综合测评成绩'.decode('utf-8'))
+		savesheet.write(0,5,'2014-15学年综合测评成绩'.decode('utf-8'))
+		savesheet.write(0,6,'姓名'.decode('utf-8'))
+		rows = sheet.nrows
+		for i in range(1,rows):
+			print "now deal with people " + str(i)
+			savesheet.write(i,0,i)
+			savesheet.write(i,1,sheet.cell_value(i,1))
+			savesheet.write(i,2,sheet.cell_value(i,2))
+			savesheet.write(i,3,sheet.cell_value(i,3))
+			savesheet.write(i,4,sheet.cell_value(i,4))
+			savesheet.write(i,5,sheet.cell_value(i,5))			
+			info = getInfo(int(sheet.cell_value(i,1)))
+			if info == None:
+				savesheet.write(i,6,'unknown')
+			else:
+				savesheet.write(i,6,info[0])
+	savebook.save('content(new).xls')
+	return 0
+	#book = xlwt.Workbook(encoding='utf-8',style_compression=0)
 	sheet = book.add_sheet('test',cell_overwrite_ok=True)
 	sheet.write(0,0,'姓名'.decode('utf-8'))
 	sheet.write(0,1,'学号'.decode('utf-8'))
@@ -55,4 +85,4 @@ def createXls():
 
 
 if __name__ == '__main__':
-	createXls()
+	dealXls()
